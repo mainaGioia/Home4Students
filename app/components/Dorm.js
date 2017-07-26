048e8248dcf749697398b9a894997b8cc61c1ee7
mainaGioia <alessandra.manna89@gmail.com>
2017-07-26 17:40:55 +0200

    last change

--- app/components/Dorm.js
+++ app/components/Dorm.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, WebView } from 'react-native';
 import { Tile, List, ListItem, Button } from 'react-native-elements';
 import { dorm } from '../config/data';
 
+class TirolerWebView extends Component{
+  render() {
+    return (
+      <WebView source={{uri: 'https://www.home4students.at/en/living/dormitories-innsbruck/74-studentenheim-technikerstrasse/'}}
+      style={{marginTop: 20}}
+      />
+    );
+  }
+}
 
 export default class Dorm extends Component{
 
   handleButtonPress = () => {
     console.log("that's navigation: ", this.props.navigation);
     console.log("dorm: ", dorm.Tirolerheim);
-    this.props.navigation.navigate('https://www.home4students.at/en/living/dormitories-innsbruck/74-studentenheim-technikerstrasse/');
+    this.props.navigation.navigate();
   };
 
   render(){
@@ -18,7 +27,7 @@ export default class Dorm extends Component{
         <Tile
           imageSrc={ require('../images/tirolerheim.jpg') }
           featured
-          caption="Tirolerheim"
+          title="TIROLERHEIM"
         />
         <Button
           title= "Website"
