393dcabd681687fd1a96907b8bb0f315e23fbeef
mainaGioia <alessandra.manna89@gmail.com>
2017-08-04 11:25:47 +0200

    moved navigation stacks in router.js

--- App.js
+++ App.js
@@ -1,52 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { TabNavigator, StackNavigator } from 'react-navigation';
-import { Icon } from 'react-native-elements';
+import { MainScreenNavigator } from './app/config/router.js';
 import { Constants } from 'expo';
-import Utils from './app/components/Utils';
-import UtilDetail from './app/components/UtilDetail';
-import SubDetail from './app/components/SubDetail';
-import Dorm from './app/components/Dorm';
 
-
-const UtilsStack = StackNavigator({
-  Utils: {
-    screen: Utils,
-    navigationOptions: { title: 'Utils' },
-  },
-  Details: {
-    screen: UtilDetail,
-  },
-  Subcategory: {
-    screen: SubDetail,
-  }
-})
-
-const MainScreenNavigator = TabNavigator({
-  Utils: {
-    screen: UtilsStack,
-    navigationOptions: {
-      tabBarLabel: 'Utils',
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name="list" size={35} color={tintColor}/>
-    },
-  },
-  Main: {
-    screen: Dorm,
-    navigationOptions: {
-      tabBarLabel: 'Infos',
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name="domain" size={35} color={tintColor}/>
-    },
-  },
-},{
-  tabBarOptions: {
-    style: {
-      width: Dimensions.get("window").width,
-      // backgroundColor: '#789fbe',
-    },
-  },
-});
 
 export default class App extends Component {
   render() {
---
+++ app/config/router.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Dimensions } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Utils from '../components/Utils';
+import UtilDetail from '../components/UtilDetail';
+import SubDetail from '../components/SubDetail';
+import Dorm from '../components/Dorm';
+
+
+const UtilsStack = StackNavigator({
+  Utils: {
+    screen: Utils,
+    navigationOptions: { title: 'Utils' },
+  },
+  Details: {
+    screen: UtilDetail,
+  },
+  Subcategory: {
+    screen: SubDetail,
+  }
+});
+
+export const MainScreenNavigator = TabNavigator({
+  Utils: {
+    screen: UtilsStack,
+    navigationOptions: {
+      tabBarLabel: 'Utils',
+      tabBarIcon: ({ tintColor }) =>
+        <Icon name="list" size={35} color={tintColor}/>
+    },
+  },
+  Main: {
+    screen: Dorm,
+    navigationOptions: {
+      tabBarLabel: 'Infos',
+      tabBarIcon: ({ tintColor }) =>
+        <Icon name="domain" size={35} color={tintColor}/>
+    },
+  },
+},{
+  tabBarOptions: {
+    style: {
+      width: Dimensions.get("window").width,
+      // backgroundColor: '#789fbe',
+    },
+  },
+});
